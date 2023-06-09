a656f487cbc1f5a6299eae5b92dab1fa87b23983
amalagon1 <89949832+amalagon1@users.noreply.github.com>
2023-06-09 11:13:38 -0400

    worked on login page

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyA5mViAX40gtjx3MLsiyUTyGHdC_11GUBY",
     authDomain: "myflix-a04d0.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
     messagingSenderId: "673931415186",
     appId: "1:673931415186:web:911d4c570bf7597e4fe04b"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useRef } from 'react';
+import './login.scss';
 
 const Login = () => {
+
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+
     return (
-        <div>
-            <h1>Log in!</h1>
+        <div className="login">
+            <div className="modal">
+                <div className="modal__content">
+                    <h1>Sign In</h1>
+                    <form>
+                        <input type="text" placeholder="Email" required="true"></input>
+                        <input type="text" placeholder="Password" required="true"></input>
+                        <button type="submit" className="login__btn">
+                            Sign In
+                        </button>
+
+                    </form>
+                    <div className="login__secondary">
+                        <p>New to Netflix?</p>
+                        <p>Sign up now.</p>
+
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
