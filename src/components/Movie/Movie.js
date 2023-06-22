a295ae00d8f50ef308def93f26133b5a500d063b
amalagon1 <89949832+amalagon1@users.noreply.github.com>
2023-06-22 15:34:30 -0400

    card styling fixed

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import './movie.scss'
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import { PlaceSharp } from '@mui/icons-material';
 
 const Movie = ({ title, image }) => {
     return (
         <div className="card-container">
             <img src={image}></img>
             <div className="card-container__lower">
-                <button className="play">
-                    <p>Play trailer</p>
+                <button className="lower-btn play">
+                    <PlayArrowIcon />
                 </button>
-                <button className="list">
+                <button className="lower-btn list">
                     +
                 </button>
             </div>
